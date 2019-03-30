7f0fe0d7d515f975bb0f4d70077a077f167100bb
Bill Dang <sparkfire.ffs@gmail.com>
2019-03-30 13:04:26 -0500

    Added basic player movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,17 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public GameObject tapMarker;
+
+    private Rigidbody2D rb;
+
+    private void Awake() {
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void FixedUpdate() {
+        if(Input.touchCount > 0) {
+            rb.velocity = Vector3.Normalize(Camera.main.ScreenToWorldPoint((Vector3)Input.GetTouch(0).position) - transform.position) * moveSpeed;
+        } else if(Input.GetMouseButton(0)) {
+            rb.velocity = Vector3.Normalize(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position) * moveSpeed;
+        } else {
+            rb.velocity = Vector3.zero;
+        }
     }
+
 }
